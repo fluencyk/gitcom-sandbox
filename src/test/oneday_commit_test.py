6498a04fc7de080449c863e4907d0c4f8a668453
Yujun Kong <244898831@qq.com>
2022-06-11 12:00:00 -0500

    noise: TDD delete oneday backfill for 2022-06-11

--- src/test/oneday_commit_test.py
+++ src/test/oneday_commit_test.py
@@ -4,14 +4,14 @@ import sys
 import subprocess
 from pathlib import Path
 
-# === ensure src/ in PYTHONPATH ===
+# === ensure src in PYTHONPATH ===
 SRC_ROOT = Path(__file__).resolve().parents[1]
 if str(SRC_ROOT) not in sys.path:
     sys.path.insert(0, str(SRC_ROOT))
 
 from core.identity import assert_identity
 from core.time_engine import clear, inject
-from mod.file_rename import file_rename
+from mod.file_delete import file_delete
 
 
 def run_oneday_commit(
@@ -20,33 +20,32 @@ def run_oneday_commit(
     allowed_emails=None,
 ):
     """
-    oneday（rename 版）：
-    1. identity
-    2. time
-    3. rename 原子行为
-    4. git add / commit / push
+    oneday (delete 版)：
+    1) 身份校验
+    2) 时间注入
+    3) delete 原子行为
+    4) git add / commit / push
     """
 
-    # --- 1. identity ---
+    # 身份
     ident = assert_identity(allowed_emails=allowed_emails)
     print(f"[identity] OK: {ident}")
 
-    # --- 2. time ---
+    # 时间
     clear()
     inject(date_str)
     print(f"[time] injected: {date_str}")
 
     repo_root = Path(__file__).resolve().parents[2]
 
-    # --- 3. rename behavior ---
-    renamed = file_rename(
+    # delete
+    deleted = file_delete(
         repo_root=repo_root,
-        old_relpath="sandbox/add_2022_06_07.txt",
-        new_relpath="sandbox/renamed_2022_06_09.txt",
+        relpath="sandbox/renamed_2022_06_09.txt",
     )
-    print(f"[rename] -> {renamed}")
+    print(f"[delete] removed: {deleted}")
 
-    # --- 4. git ops ---
+    # git ops
     def git(cmd):
         subprocess.check_call(cmd, cwd=repo_root)
 
@@ -59,7 +58,7 @@ def run_oneday_commit(
 
 if __name__ == "__main__":
     run_oneday_commit(
-        date_str="2022-06-09 12:00:00 -0500",
-        message="noise: TDD rename oneday backfill for 2022-06-09",
+        date_str="2022-06-11 12:00:00 -0500",
+        message="noise: TDD delete oneday backfill for 2022-06-11",
         allowed_emails={"244898831@qq.com"},
     )
